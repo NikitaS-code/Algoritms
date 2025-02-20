222782fdc54c0cc9f98181f9ec75790f336d00b6
NikitaS-code <nikitastalintsev@yandex.ru>
2025-02-20 13:27:11 +0500

    Add files via upload

---
+++ 2_1.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            a=nums.index(target)
+            return a      
